ab1c4319f857bf594b11d9f9226802118527d2a0
Lalo Luna <57363601+laloluna14@users.noreply.github.com>
2021-02-27 14:06:38 -0600

    Added dictionary handler

---
+++ 02_lista_diccionario.py
@@ -0,0 +1,31 @@
+informacion = {
+    'students': [
+        {
+            'name': 'Galileo Guzman',
+            'age': 31,
+            'hobbies': ['music', 'movies', 'biking', 'read', 'books']
+        },
+        {
+            'name': 'Omar Flores',
+            'age': 18,
+            'hobbies': ['troba', 'movies', 'biking', 'read', 'books']
+        },
+        {
+            'name': 'Armando',
+            'age': 18,
+            'hobbies': ['music', 'read', 'books']
+        },
+        {
+            'name': 'Eduardo',
+            'age': 18,
+            'hobbies': ['music', 'read', 'books']
+        },
+    ],
+    'last_update': '2021-02-27',
+    'modified_by': 'frasgado@mail.io'
+}
+
+print(type(informacion))
+print(type(informacion['students']))
+print(type(informacion['last_update']))
+print(type(informacion['modified_by']))
